9c062a9fd1a62af50d18217b2fedead3473e64e0
elvischikudzah <elvischikudza@gmail.com>
2023-10-09 06:09:52 -0700

    fooled

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,18 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-printf("The number %d is ", n);
+printf("%d is ", n);
 
 if (n > 0)
+
 printf("positive\n");
+
 else if (n == 0)
+
 printf("zero\n");
+
 else
+
 printf("negative\n");
 
 return (0);
