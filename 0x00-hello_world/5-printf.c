38676e551ee566a1bf740267e4b5f03a3bd468ae
elvischikudzah <elvischikudza@gmail.com>
2023-10-09 06:28:20 -0700

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+* main - Entry point, prints whether a random number is positive, negative, or zero
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
