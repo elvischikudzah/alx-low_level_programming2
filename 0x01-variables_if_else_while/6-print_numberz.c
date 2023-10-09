9295882e1188f8c45b991e23c5e33b590ebb698f
elvischikudzah <elvischikudza@gmail.com>
2023-10-09 05:21:33 -0700

    Good

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* main - Entry point
+*
+* Description: Prints single-digit numbers from 0 to 9 using putchar.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int number = 0;
+while (number < 10)
+{
+putchar(number + '0');
+number++;
+}
+putchar('\n');
+return (0);
+}
+
